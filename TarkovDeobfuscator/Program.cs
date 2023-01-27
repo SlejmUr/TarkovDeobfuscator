c38fd6c6c38150cf3bd91826e4f7638e62b20add
SlejmUr <slejmur@protonmail.com>
2023-01-27 18:12:37 +0100

    ,

--- TarkovDeobfuscator/Program.cs
+++ TarkovDeobfuscator/Program.cs
@@ -8,22 +8,22 @@
             {
                 if (args[0].Contains("-remap"))
                 {
-                    Deobf.DeobfuscateAssembly("Assembly-CSharp.dll", "Managed", true, false, true);
+                    Deobf.DeobfuscateAssembly("EscapeFromTarkov_Data/Managed/Assembly-CSharp.dll", "EscapeFromTarkov_Data/Managed", true, false, true);
                     return;
                 }
                 if (args[0].Contains("-override"))
                 {
-                    Deobf.DeobfuscateAssembly("Assembly-CSharp.dll", "Managed", true, true, false);
+                    Deobf.DeobfuscateAssembly("EscapeFromTarkov_Data/Managed/Assembly-CSharp.dll", "EscapeFromTarkov_Data/Managed", true, true, false);
                     return;
                 }
                 if (args[0].Contains("-both"))
                 {
-                    Deobf.DeobfuscateAssembly("Assembly-CSharp.dll", "Managed", true, true, true);
+                    Deobf.DeobfuscateAssembly("EscapeFromTarkov_Data/Managed/Assembly-CSharp.dll", "EscapeFromTarkov_Data/Managed", true, true, true);
                     return;
                 }
             }
 
-            Deobf.DeobfuscateAssembly("Assembly-CSharp.dll", "Managed");
+            Deobf.DeobfuscateAssembly("EscapeFromTarkov_Data/Managed/Assembly-CSharp.dll", "EscapeFromTarkov_Data/Managed", true, true, true);
         }
     }
 }
