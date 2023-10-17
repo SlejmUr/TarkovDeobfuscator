7af5b9d53385ef83c4c56d10a46ff9c385c9e67b
SlejmUr <slejmur@protonmail.com>
2023-10-17 13:16:32 +0200

    yeet

--- TarkovDeobfuscator/Program.cs
+++ TarkovDeobfuscator/Program.cs
@@ -4,6 +4,9 @@
     {
         static void Main(string[] args)
         {
+            Deobf.OnLog += Deobf_OnLog;
+
+
             if (args.Length != 0)
             {
                 if (args[0].Contains("-remap"))
@@ -24,6 +27,18 @@
             }
 
             Deobf.DeobfuscateAssembly("EscapeFromTarkov_Data/Managed/Assembly-CSharp.dll", "EscapeFromTarkov_Data/Managed", true, true, true);
+        }
+
+        private static void Deobf_OnLog(string text)
+        {
+            if (!File.Exists("deobf_log.txt"))
+            {
+                File.WriteAllText("deobf_log.txt", text);
+            }
+            else
+            {
+                File.AppendAllText("deobf_log.txt", text + "\n");
+            }
         }
     }
 }
